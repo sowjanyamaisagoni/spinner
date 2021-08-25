872f5267831f5709fe94978f8f75840a27035ee7
Sowjanya Maisagoni <maisagoni@gmail.com>
2021-08-25 11:54:24 -0400

    adding |

--- spinner1.js
+++ spinner1.js
@@ -14,5 +14,7 @@ setTimeout(() => {
    // Need to escape the backslash since it's a special character.
    process.stdout.write('\r\\   '); 
  }, 700);
+ setTimeout(() => {
+   process.stdout.write('\r|   ');
+ }, 1000);
 
- 
