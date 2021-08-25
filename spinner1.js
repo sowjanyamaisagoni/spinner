ed94cb41bb6b511bd00fd4bf120a0d3adfca59d2
Sowjanya Maisagoni <maisagoni@gmail.com>
2021-08-25 11:12:06 -0400

    adding spinner

---
+++ spinner1.js
@@ -0,0 +1,18 @@
+setTimeout(() => {
+   process.stdout.write('\r|   ');
+ }, 100);
+ 
+ setTimeout(() => {
+   process.stdout.write('\r/   ');
+ }, 300);
+ 
+ setTimeout(() => {
+   process.stdout.write('\r-   ');
+ }, 500);
+ 
+ setTimeout(() => {
+   // Need to escape the backslash since it's a special character.
+   process.stdout.write('\r\\   '); 
+ }, 700);
+
+ 
